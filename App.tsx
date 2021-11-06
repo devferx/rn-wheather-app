9d72b902cbe3262fa24a4c9468708a661f32bd96
devferx <ferkki@hotmail.com>
2021-11-06 16:23:30 -0400

    :construction: [UPDATE] Created BottomSheet Component File
    Moved from App.tsx to BottomSheet.tsx

--- App.tsx
+++ App.tsx
@@ -1,36 +1,19 @@
-import 'react-native-gesture-handler';
-import React, {useCallback, useMemo, useRef} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {ImageBackground, StyleSheet, Text} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import BottomSheet from '@gorhom/bottom-sheet';
-
-import {ImageBackground, StyleSheet} from 'react-native';
 
 import {CardInfo} from './src/components/CardInfo';
+import {BottomSheet} from './src/components/BottomSheet';
 
 const App = () => {
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-  const snapPoints = useMemo(() => [32, '50%'], []);
-
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
-
   return (
     <GestureHandlerRootView style={styles.container}>
       <ImageBackground
         source={require('./src/assets/bg.png')}
         style={styles.container}>
         <CardInfo temp={25} date={new Date()} location="Cochabamba, Bolivia" />
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={0}
-          snapPoints={snapPoints}
-          onChange={handleSheetChanges}>
-          <View style={styles.contentContainer}>
-            <Text>Awesome 🎉</Text>
-          </View>
+        <BottomSheet>
+          <Text style={{color: 'white'}}>Hello World</Text>
         </BottomSheet>
       </ImageBackground>
     </GestureHandlerRootView>
@@ -40,10 +23,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  contentContainer: {
-    flex: 1,
-    alignItems: 'center',
   },
 });
 
---
+++ src/components/BottomSheet.tsx
@@ -0,0 +1,44 @@
+import React, {useMemo, useRef} from 'react';
+import {StyleSheet} from 'react-native';
+import BottomSheetContainer, {
+  BottomSheetScrollView,
+} from '@gorhom/bottom-sheet';
+
+type BottomSheetProps = {
+  children?: React.ReactNode | null;
+};
+
+export const BottomSheet = ({children}: BottomSheetProps) => {
+  const bottomSheetRef = useRef<BottomSheetContainer>(null);
+
+  const snapPoints = useMemo(() => [32, '50%'], []);
+
+  return (
+    <BottomSheetContainer
+      ref={bottomSheetRef}
+      index={0}
+      snapPoints={snapPoints}
+      backgroundStyle={styles.backgroundStyle}
+      handleIndicatorStyle={styles.handleIndicatorStyle}>
+      <BottomSheetScrollView style={styles.contentContainer}>
+        {children}
+      </BottomSheetScrollView>
+    </BottomSheetContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#1F1F1F',
+  },
+  handleIndicatorStyle: {
+    backgroundColor: '#EFF0F3',
+    width: 64,
+    height: 4,
+  },
+  contentContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    flex: 1,
+  },
+});
